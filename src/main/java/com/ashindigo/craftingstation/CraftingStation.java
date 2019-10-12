343ac46185102e26faa91d6639de40bf3f8278d4
Ash <5542898+AshIndigo@users.noreply.github.com>
2019-10-12 01:44:43 -0400

    Initial Commit

---
+++ src/main/java/com/ashindigo/craftingstation/CraftingStation.java
@@ -0,0 +1,13 @@
+package com.ashindigo.craftingstation;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CraftingStation implements ModInitializer {
+
+    public static final String MODID = "craftingstation";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
