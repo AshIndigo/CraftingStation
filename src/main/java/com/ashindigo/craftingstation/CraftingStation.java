539b28392469ce0dc0d71dd293657cbd215d183a
Ash <5542898+AshIndigo@users.noreply.github.com>
2019-11-02 18:34:03 -0400

    Thanks intellij

--- src/main/java/com/ashindigo/craftingstation/CraftingStation.java
+++ src/main/java/com/ashindigo/craftingstation/CraftingStation.java
@@ -28,7 +28,7 @@ public class CraftingStation implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Block craftingStationBlock = new CraftingStationBlock(FabricBlockSettings.of(Material.WOOD).strength(2.5F, 2.5F).drops(new Identifier(MODID, MODID)).sounds(BlockSoundGroup.WOOD).build());
+        Block craftingStationBlock = new CraftingStationBlock(FabricBlockSettings.of(Material.WOOD).strength(2.5F, 2.5F).sounds(BlockSoundGroup.WOOD).build());
         Registry.register(Registry.BLOCK, craftingStationID, craftingStationBlock);
         Registry.register(Registry.ITEM, craftingStationID, new BlockItem(craftingStationBlock, new Item.Settings().maxCount(64).group(ItemGroup.MISC)));
         type = BlockEntityType.Builder.create(CraftingStationTileEntity::new, craftingStationBlock).build(null);
